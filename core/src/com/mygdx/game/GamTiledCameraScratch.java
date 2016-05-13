1ad9e235838fb8515bd0c418f03a8b7056e541b7
Ashleigh <ashcstroud@hotmail.com>
2016-05-13 10:49:51 -0400

    Got map bounds to work using camera and sprite clamping

--- core/src/com/mygdx/game/GamTiledCameraScratch.java
+++ core/src/com/mygdx/game/GamTiledCameraScratch.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
@@ -30,15 +31,14 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 	int nHeight;
 	int nWidth;
 
-	int nTileLayerHeight;
-	int nTileLayerWidth;
+	int nMapWidth, nMapHeight, nTileWidth, nTileHeight, nMapTileWidth, nMapTileHeight;
 	SpriteBatch sbBatch;
 	Texture txSprite;
 	//Texture BackGround;
 	TextureRegion[] artrFrames;
 	TextureRegion trCurrentFrame;
-	float fSpriteX = 100;
-	float fSpriteY = 100;
+	float fSpriteX = 0;
+	float fSpriteY = 0;
 	float fSpriteSpeed = 100f;
 	float fTime = 0f;
 	Animation aniMain;
@@ -51,14 +51,14 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 	String sDirection;
 	Object objProperty;
 	TiledMapTileLayer.Cell cCurrentCell;
-	MapProperties mpWalkable;
+	MapProperties mpBounds;
 	TiledMapTileLayer tmtlBounds;
 
 	@Override
 	public void create() {
 		rectSprite = new Rectangle();
 		sbBatch = new SpriteBatch();
-		mpWalkable = new MapProperties();
+		mpBounds = new MapProperties();
 		//BackGround = new Texture(Gdx.files.internal("lostwoods2.jpg"));
 		txSprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
 		TextureRegion[][] tmp = TextureRegion.split(txSprite, txSprite.getWidth() / nCols, txSprite.getHeight() / nRows);
@@ -80,14 +80,24 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 		tmGameMap = new TmxMapLoader().load("CameraPanMap.tmx");
 		orthotmrRenderer = new OrthogonalTiledMapRenderer(tmGameMap);
 
+		mpBounds = tmGameMap.getProperties();
+
+		nMapWidth = mpBounds.get("width", Integer.class);
+		nMapHeight = mpBounds.get("height", Integer.class);
+		nTileWidth = mpBounds.get("tilewidth", Integer.class);
+		nTileHeight = mpBounds.get("tileheight", Integer.class);
+
+		nMapTileWidth = nMapWidth * nTileWidth;
+		nMapTileHeight = nMapHeight * nTileHeight;
+
 		System.out.println("Screen Height:" + Gdx.graphics.getHeight());
 		nHeight = Gdx.graphics.getHeight();
 		nWidth = Gdx.graphics.getWidth();
 
 		tmtlBounds = (TiledMapTileLayer) tmGameMap.getLayers().get("Foreground");
-		nTileLayerHeight = tmtlBounds.getHeight();
-		nTileLayerWidth = tmtlBounds.getWidth();
-		/*for (int i = 0; i < tmtlBounds.getWidth(); i++) {
+		//nTileLayerHeight = tmtlBounds.getHeight();
+		//nTileLayerWidth = tmtlBounds.getWidth();
+		for (int i = 0; i < tmtlBounds.getWidth(); i++) {
 			for (int j = 0; j < tmtlBounds.getHeight(); j++) {
 				if (tmtlBounds.getCell(i, j) != null) {
 					//tmtlBounds.getProperties().get("Walkable");
@@ -99,8 +109,7 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 					}
 				}
 			}
-		}*/
-		FindBounds();
+		}
 	}
 
 	@Override
@@ -141,10 +150,19 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 			sDirection = "Down";
 		}
 
-		//Rendering Tiled Map
+		ocMainCam.position.set(fSpriteX, fSpriteY, 0);
+		sbBatch.setProjectionMatrix(ocMainCam.combined);
+		ocMainCam.position.x = MathUtils.clamp(ocMainCam.position.x, 0 + (Gdx.graphics.getWidth() / 2), nMapTileWidth - (Gdx.graphics.getWidth() / 2));
+		ocMainCam.position.y = MathUtils.clamp(ocMainCam.position.y, 0 + (Gdx.graphics.getHeight() / 2), nMapTileHeight - (Gdx.graphics.getHeight() / 2));
 		ocMainCam.update();
+
+		fSpriteX = MathUtils.clamp(fSpriteX, 0 + 16, nMapTileWidth - 16);
+		fSpriteY = MathUtils.clamp(fSpriteY, 0 + 32, nMapTileHeight -32);
+
+		//Rendering Tiled Map
 		orthotmrRenderer.setView(ocMainCam);
 		orthotmrRenderer.render();
+		ocMainCam.update();
 
 		//Draw Sprites
 		sbBatch.begin();
@@ -152,11 +170,11 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 		sbBatch.draw(trCurrentFrame, (int) fSpriteX, (int) fSpriteY);
 		sbBatch.end();
 
-		if (fSpriteY > nHeight) {
-			//RemoveBoundsOnScreen();
+
+		/*if (fSpriteY > nHeight) {
+			RemoveBoundsOnScreen();
 			ocMainCam.translate(0, nHeight, 0);
 			fSpriteY = 0;
-			//FindBoundsOnScreen();
 		} else if (fSpriteY < 0) {
 			RemoveBoundsOnScreen();
 			ocMainCam.translate(0, 0 - nHeight, 0);
@@ -172,8 +190,8 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 			ocMainCam.translate(0 - nWidth, 0, 0);
 			fSpriteX = nWidth;
 			//FindBoundsOnScreen();
-		}
-		for (int i = 0; i < arlRectCollisionDetection.size(); i++) {
+		}*/
+		/*for (int i = 0; i < arlRectCollisionDetection.size(); i++) {
 			if (rectSprite.overlaps(arlRectCollisionDetection.get(i))) {
 				System.out.println("Collision detected!");
 				System.out.println(sDirection);
@@ -189,53 +207,8 @@ public class GamTiledCameraScratch extends ApplicationAdapter {
 			}
 		}
 	}
-	public void RemoveBoundsOnScreen() {
-		for(int i = 0; i < arlRectCollisionDetection.size(); i++){
-			if(arlRectCollisionDetection.get(i).getX() <= nWidth && arlRectCollisionDetection.get(i).getY() <= nHeight){
-			arlRectCollisionDetection.remove(i);
-			}
-		}
-	}
-	public void FindBounds(){
-		for (int i = 0; i < nTileLayerWidth; i++) {
-			for (int j = 0; j < nTileLayerHeight; j++) {
-				if (tmtlBounds.getCell(i, j) != null) {
-					//tmtlBounds.getProperties().get("Walkable");
-					cCurrentCell = tmtlBounds.getCell(i, j);
-					objProperty = cCurrentCell.getTile().getProperties().containsKey("Blocked");
-					if (objProperty != null) {
-						System.out.println("Rectangle added!");
-						arlRectCollisionDetection.add(new Rectangle(i * 64, j * 64, 32, 32));
-					}
-				}
-			}
-		}
-
-	}
-	public void FindBoundsOnScreen(){
-		for (int i = 0; i < tmtlBounds.getWidth(); i++) {
-			for (int j = 0; j < tmtlBounds.getHeight(); j++) {
-				if (tmtlBounds.getCell(i, j) != null) {
-					//tmtlBounds.getProperties().get("Walkable");
-					cCurrentCell = tmtlBounds.getCell(i, j);
-					objProperty = cCurrentCell.getTile().getProperties().containsKey("Blocked");
-					if (objProperty != null) {
-						arlRectCollisionDetection.add(new Rectangle(i * 64, j * 64, 32, 32));
-					}
-				}
-			}
-		}
+}*/
 	}
 }
-	/*public boolean isbBoundsCheck(){
-		for (int i = 0; i < arlRectObjectBounds.size(); i++) {
-			if(rectSprite.overlaps(arlRectObjectBounds.get(i))) {
-				System.out.println("Collision Detected");
-				bBoundsCheck=true;
-				return bBoundsCheck;
-			}
-		}
-		bBoundsCheck=false;
-		return bBoundsCheck;
-	}
-}*/
+
+
